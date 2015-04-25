c23606f57110361dbd632d5683326d53a78744c7
adrianaene <adriana.ene93@gmail.com>
2015-04-25 15:55:34 +0300

    comit2

---
+++ Linq/Membru.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Linq
+{
+    public class Membru
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int BandId { get; set; }
+
+        public Membru(int id, string name, int bandId)
+        {
+            this.Id = id;
+            this.Name = name;
+            this.BandId = bandId;
+        }
+    }
+}
